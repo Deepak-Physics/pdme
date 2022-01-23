da7892adbdf3a3a7de8a451db8b5a0c7a24126a2
Deepak Mallubhotla <dmallubhotla+github@gmail.com>
2022-01-23 16:25:29 -0600

    Adds discretisation to import path

--- pdme/model/__init__.py
+++ pdme/model/__init__.py
@@ -1,5 +1,5 @@
-from pdme.model.model import Model
+from pdme.model.model import Model, Discretisation
 from pdme.model.fixed_z_plane_model import FixedZPlaneModel
 from pdme.model.unrestricted_model import UnrestrictedModel
 
-__all__ = ["Model", "FixedZPlaneModel", "UnrestrictedModel"]
+__all__ = ["Model", "Discretisation", "FixedZPlaneModel", "UnrestrictedModel"]
