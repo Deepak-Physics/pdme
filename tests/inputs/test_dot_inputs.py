852836b924c8ffefd6b28a8ea1bc1ca47b77756e
Deepak Mallubhotla <dmallubhotla+github@gmail.com>
2022-03-27 16:31:03 -0500

    feat: adds methods for better creation of input lists, including pairs

---
+++ tests/inputs/test_dot_inputs.py
@@ -0,0 +1,31 @@
+import pdme.inputs
+
+
+def test_inputs_with_frequency_range():
+	dot1 = [1, 2, 3]
+	dot2 = [2, 4, 6]
+
+	frequencies = [5, 7, 9]
+
+	expected = [
+		([1, 2, 3], 5), ([1, 2, 3], 7), ([1, 2, 3], 9), ([2, 4, 6], 7), ([2, 4, 6], 9), ([2, 4, 6], 5)
+	]
+
+	actual = pdme.inputs.inputs_with_frequency_range([dot1, dot2], frequencies)
+
+	assert sorted(actual) == sorted(expected), "Did not actually match dot inputs!"
+
+
+def test_input_pairs_with_frequency_range():
+	dot1 = [1, 2, 3]
+	dot2 = [2, 4, 6]
+
+	frequencies = [5, 7, 9]
+
+	expected = [
+		([1, 2, 3], [2, 4, 6], 5), ([1, 2, 3], [2, 4, 6], 7), ([1, 2, 3], [2, 4, 6], 9)
+	]
+
+	actual = pdme.inputs.input_pairs_with_frequency_range([dot1, dot2], frequencies)
+
+	assert sorted(actual) == sorted(expected), "Did not actually match dot inputs!"
