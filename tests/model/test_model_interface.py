bbfbfa359aa436a86565dd2172794ec164a71b51
Deepak Mallubhotla <dmallubhotla+github@gmail.com>
2022-04-30 11:51:28 -0500

    style: Removes unused function

--- tests/model/test_model_interface.py
+++ tests/model/test_model_interface.py
@@ -5,16 +5,12 @@ import pytest
 def test_model_interface_not_implemented_one_dipoles():
 	model = DipoleModel()
 
-	model.point_length = lambda: 2
-
 	with pytest.raises(NotImplementedError):
 		model.get_dipoles(5)
 
 
 def test_model_interface_not_implemented_n_dipoles():
 	model = DipoleModel()
-
-	model.point_length = lambda: 2
 
 	with pytest.raises(NotImplementedError):
 		model.get_monte_carlo_dipole_inputs(5, 10)
