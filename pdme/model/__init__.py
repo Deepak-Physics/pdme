0c64ed02f0d42cd0957be0511a35dc49153a256f
Deepak Mallubhotla <dmallubhotla+github@gmail.com>
2022-04-30 16:20:39 -0500

    feat: adds multidipole fixed mag model

--- pdme/model/__init__.py
+++ pdme/model/__init__.py
@@ -1,7 +1,9 @@
 from pdme.model.model import DipoleModel
 from pdme.model.fixed_magnitude_model import SingleDipoleFixedMagnitudeModel
+from pdme.model.multidipole_fixed_magnitude_model import MultipleDipoleFixedMagnitudeModel
 
 __all__ = [
 	"DipoleModel",
 	"SingleDipoleFixedMagnitudeModel",
+	"MultipleDipoleFixedMagnitudeModel",
 ]
