1bad2f743af6a95189448d8929c70a036e6c21ab
Deepak Mallubhotla <dmallubhotla+github@gmail.com>
2022-04-30 14:51:11 -0500

    feat: adds fast method for calculating multiple dipole calculations

--- pdme/util/fast_v_calc.py
+++ pdme/util/fast_v_calc.py
@@ -10,6 +10,7 @@ def fast_vs_for_dipoles(
 ) -> numpy.ndarray:
 	"""
 	No error correction here baby.
+	Expects dot_inputs to be numpy array of [rx, ry, rz, f] entries, so a n by 4 where n is number of measurement points.
 	"""
 	ps = dipoles[:, 0:3]
 	ss = dipoles[:, 3:6]
@@ -33,6 +34,39 @@ def fast_vs_for_dipoles(
 	bses = (1 / numpy.pi) * (ws[:, None] / (fs**2 + ws[:, None] ** 2))
 	_logger.debug(f"bses: {bses}")
 	return ases * bses
+
+
+def fast_vs_for_dipoleses(
+	dot_inputs: numpy.ndarray, dipoleses: numpy.ndarray
+) -> numpy.ndarray:
+	"""
+	No error correction here baby.
+	Expects dot_inputs to be numpy array of [rx, ry, rz, f] entries, so a n by 4 where n is number of measurement points.
+
+	Dipoleses are expected to be array of arrays of arrays: list of sets of dipoles which are part of a single arrangement to be added together.
+	"""
+	ps = dipoleses[:, :, 0:3]
+	ss = dipoleses[:, :, 3:6]
+	ws = dipoleses[:, :, 6]
+
+	_logger.debug(f"ps: {ps}")
+	_logger.debug(f"ss: {ss}")
+	_logger.debug(f"ws: {ws}")
+
+	rs = dot_inputs[:, 0:3]
+	fs = dot_inputs[:, 3]
+
+	diffses = rs[:, None] - ss[:, None, :]
+
+	_logger.debug(f"diffses: {diffses}")
+	norms = numpy.linalg.norm(diffses, axis=3) ** 3
+	_logger.debug(f"norms: {norms}")
+	ases = (numpy.einsum("abcd,acd->abc", diffses, ps) / norms) ** 2
+	_logger.debug(f"ases: {ases}")
+
+	bses = (1 / numpy.pi) * (ws[:, None, :] / (fs[:, None]**2 + ws[:, None, :] ** 2))
+	_logger.debug(f"bses: {bses}")
+	return numpy.einsum("...j->...", ases * bses)
 
 
 def between(a: numpy.ndarray, low: numpy.ndarray, high: numpy.ndarray) -> numpy.ndarray:
--- tests/util/test_fast_v_calc.py
+++ tests/util/test_fast_v_calc.py
@@ -24,6 +24,28 @@ def test_fast_v_calc():
 	)
 
 
+def test_fast_v_calc_multidipoles():
+	d1 = [1, 2, 3, 4, 5, 6, 7]
+	d2 = [2, 5, 3, 4, -5, -6, 2]
+
+	dipoles = numpy.array([[d1, d2]])
+
+	dot_inputs = numpy.array([[-1, -1, -1, 11], [2, 3, 1, 5.5]])
+	# expected_ij is for dot i, dipole j
+	expected_11 = 0.00001421963287022476
+	expected_12 = 0.00001107180225755457
+	expected_21 = 0.000345021108583681380388722
+	expected_22 = 0.0000377061050587914705139781
+
+	expected = numpy.array([[expected_11 + expected_12, expected_21 + expected_22]])
+
+	numpy.testing.assert_allclose(
+		pdme.util.fast_v_calc.fast_vs_for_dipoleses(dot_inputs, dipoles),
+		expected,
+		err_msg="Voltages at dot aren't as expected for multidipole calc.",
+	)
+
+
 def test_between():
 	low = numpy.array([1, 2, 3])
 	high = numpy.array([6, 7, 8])
